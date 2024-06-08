54690ef3cd5019d48ca437a2be8292b135d059a9
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-06-08 16:30:16 +0530

    feat: Add todo's to routes and controllers for dashboards, tweets, videos, comments, subscriptions, and likes

---
+++ src/controllers/subscription.controllers.js
@@ -0,0 +1,27 @@
+import mongoose,{isValidObjectId} from "mongoose"
+import {User} from "../models/user.models.js"
+import { Subscription } from "../models/subscription.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    // TODO: toggle subscription
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
---
+++ src/controllers/tweet.controllers.js
@@ -0,0 +1,29 @@
+import mongoose,{isValidObjectId} from "mongoose"
+import {Tweet} from "../models/tweet.models.js"
+import {User} from "../models/user.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
