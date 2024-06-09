cd7b07ededd6ae93c26fd4c7f109d4625eb97ac1
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-06-09 19:26:47 +0530

    feat: Add channel stats and videos endpoints,controllers

--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -8,10 +8,52 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const {channelId} = req.params
+    if(!mongoose.isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid channel id")
+    }
+    const totalVideos = await Video.countDocuments({channel:channelId})
+    const totalSubscribers = await Subscription.countDocuments({channel:channelId})
+    const totalLikes = await Like.countDocuments({channel:channelId})
+    const totalViews = await Video.aggregate([
+        {
+            $match:{channel:channelId}
+        },
+        {
+            $group:{ 
+                _id:null,
+                totalViews:{$sum:"$views"}
+            }
+        }
+    ])
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,{totalVideos,totalSubscribers,totalLikes,totalViews},"Channel stats are fetched successfully"))
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const {channelId} = req.params
+    const {page = 1, limit = 10} = req.query
+    if(!mongoose.isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid channel id")
+    }
+    const videos = await Video.aggregatePaginate(
+        {channel:channelId},
+        {
+            page:parseInt(page),
+            limit:parseInt(limit),
+            sort:{createdAt:"desc"},
+            customLabels:{
+                docs:"videos"
+            }
+        }
+    )
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,{videos},"Videos are fetched successfully"))
 })
 
 export {
