ac243235eba4dc63172d2607e712ca4606b92cfa
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-06-09 19:29:31 +0530

    feat: Add error handling and validation for channel subscriber endpoints and controllers

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -13,11 +13,35 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid channel id")
+    }
+    const channel = await User.findById(channelId)
+    if(!channel){
+        throw new ApiError(404,"Channel not found")
+    }
+    const subscribers = await Subscription.find({channel:channelId})
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,{subscribers},"Subscribers are fetched successfully"))
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if(!isValidObjectId(subscriberId)){
+        throw new ApiError(400,"Invalid subscriber id")
+    }
+    const user = await User.findById(subscriberId)
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+    const subscribedChannels = await Subscription.find({subscriber:subscriberId})
+    res
+    .status(200)
+    .json(new ApiResponse(200,{subscribedChannels},"Subscribed channels are fetched successfully"))
 })
 
 export {
