aa19e3f16523299b207a6282ec1a9a0bd8562ee1
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-06-12 22:26:55 +0530

    refactor: Update dashboard.routes.js to include channelId in stats and videos routes

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/stats").get(getChannelStats);
-router.route("/videos").get(getChannelVideos);
+router.route("/stats/:channelId").get(getChannelStats);
+router.route("/videos/:channelId").get(getChannelVideos);
 
 export default router
